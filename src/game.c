5263ceddf4683f246c274189f38918c497e9c1ca
marcelochaves95 <marcelochaves17@gmail.com>
2021-10-31 22:06:41 -0300

    Read map

--- src/game.c
+++ src/game.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void space() {
+    printf("\n");
+}
 
 int main() {
-    char map[5][10];
+    char map[5][10 + 1];
+
+    char filename[] = "../files/map.txt";
+    char* path = realpath(filename, NULL);
+    if (path == NULL) {
+        printf("Database not available.\n");
+        space();
+        exit(1);
+    }
+
+    FILE* file = fopen(path, "r");
+    if (file == 0) {
+        printf("Database not available.\n");
+        space();
+        exit(1);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        fscanf(file, "%s", map[i]);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%s\n", map[i]);
+    }
     
+    fclose(file);
+
     return 0;
 }
