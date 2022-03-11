c37c6349236f6f11f6cd2d7d0d0315b684702e3e
marcelochaves95 <marcelochaves17@gmail.com>
2022-03-11 02:59:27 -0300

    Draw images

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 // gcc -o Pacman main.c -lallegro -lallegro_image -lallegro_dialog -lallegro_main
 // ./Pacman
 
-// Objetos
 struct obj {
     int wx;
     int wy;
@@ -18,18 +17,16 @@ struct obj {
 
 struct obj p = { 42, 0, 13, 23, 42, 44, 2 };
 
-// Funções
 ALLEGRO_BITMAP* load_bitmap_at_size(const char* file_name, int width, int height);
 void sair();
 void mapa(int mod);
 
-// Variáveis globais
-int width = 677;
-int height = 665;
+int width = 1048;
+int height = 670;
 int is_running = 1;
 
-const int LENGTH_X = 28;
 const int LENGTH_Y = 31;
+const int LENGTH_X = 28;
 int map [LENGTH_Y][LENGTH_X];
 
 ALLEGRO_BITMAP* buffer = NULL;
@@ -37,22 +34,25 @@ ALLEGRO_BITMAP* itens = NULL;
 ALLEGRO_BITMAP* fundo = NULL;
 
 int main() {
+    al_init_image_addon();
+
     ALLEGRO_DISPLAY* display;
     ALLEGRO_EVENT_QUEUE* event_queue;
-    ALLEGRO_BITMAP* background_bitmap = NULL;
     
     al_init();
-    display = al_create_display(width * 2, height * 2);
+    display = al_create_display(width, height);
     al_set_window_title(display, "Pacman");
-    
+
     event_queue = al_create_event_queue();
     al_install_keyboard();
     al_register_event_source(event_queue, al_get_keyboard_event_source());
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_init_image_addon();
 
-    background_bitmap = load_bitmap_at_size("../img/background.bmp", width * 2, height * 2);
-    assert(background_bitmap != NULL);
+    fundo = load_bitmap_at_size("../img/fundo.bmp", width, height);
+    itens = load_bitmap_at_size("../img/itens2.bmp", 8, 8);
+    //itens = al_load_bitmap("");
+    assert(fundo != NULL);
 
     float x = 0;
     int width = al_get_display_width(display);
@@ -60,12 +60,11 @@ int main() {
     mapa(1);
     while (is_running) {
         al_clear_to_color(al_map_rgba_f(1, 1, 1, 1));
-        al_draw_bitmap(background_bitmap, 0, 0, 0);
-        al_flip_display();
-
+        al_draw_bitmap(fundo, 0, 0, 0);
         mapa(0);
+        al_flip_display();
         if (x > width) {
-            x = -al_get_bitmap_width(background_bitmap);
+            x = -al_get_bitmap_width(fundo);
         }
 
         if (!al_is_event_queue_empty(event_queue)) {
@@ -80,44 +79,40 @@ int main() {
 
     al_destroy_display(display);
     al_uninstall_keyboard();
-    al_destroy_bitmap(background_bitmap);
+    //al_destroy_bitmap(fundo);
+    al_destroy_bitmap(itens);
 
     return 0;
 }
 
-END_OF_MAIN();
+//END_OF_MAIN();
 
 ALLEGRO_BITMAP* load_bitmap_at_size(const char* file_name, int width, int height) {
     ALLEGRO_BITMAP* resized_bitmap;
     ALLEGRO_BITMAP* loaded_bitmap;
     ALLEGRO_BITMAP* previous_target;
 
-    // Create a temporary bitmap of size we want
     resized_bitmap = al_create_bitmap(width, height);
     if (!resized_bitmap) return NULL;
 
-    // Load the bitmap at the original size
     loaded_bitmap = al_load_bitmap(file_name);
     if (!loaded_bitmap) {
         al_destroy_bitmap(resized_bitmap);
         return NULL;
     }
 
-    // Set the target bitmap to the resized bmp
     previous_target = al_get_target_bitmap();
     al_set_target_bitmap(resized_bitmap);
 
-    // Copy the loaded bitmap to the resized bmp
     al_draw_scaled_bitmap(loaded_bitmap,
-        0, 0,                                // source origin
-        al_get_bitmap_width(loaded_bitmap),  // source width
-        al_get_bitmap_height(loaded_bitmap), // source height
-        0, 0,                                // target origin
-        width, height,                       // target dimensions
-        0                                    // flags
+        0, 0,
+        al_get_bitmap_width(loaded_bitmap),
+        al_get_bitmap_height(loaded_bitmap),
+        0, 0,
+        width, height,
+        0
     );
 
-    // Restore the previous target and clean up
     al_set_target_bitmap(previous_target);
     al_destroy_bitmap(loaded_bitmap);
 
@@ -169,16 +164,17 @@ void mapa(int mod) {
 
             if (map[i][j] == 2)
             {
+                al_draw_bitmap(itens, j * LENGTH_X + 5, i * 21 + 5, 0);
+                
                 // Pontos
-                masked_blit(itens, buffer, 3, 10, j * LENGTH_X + 10, 21 + 16, 6, 6);
+                //masked_blit(itens, buffer, 3, 10, j * LENGTH_X + 10, 21 + 16, 6, 6);
             }
         }
     }
-    
 }
 
 void sair() {
     is_running = 0;
 }
 
-END_OF_FUNCTION(sair);
+//END_OF_FUNCTION(sair);
